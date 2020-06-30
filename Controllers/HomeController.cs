ac5781bf9eca2bebefe4466aad064e70ceb6883b
JosseLardinois <josse.lardinois@hotmail.nl>
2020-06-30 19:12:41 +0200

    Home Screen zichbaar voor alle gebruikers, Login en Registreer buttons weer toegevoegd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ApexLegendsTeamBuilder.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
